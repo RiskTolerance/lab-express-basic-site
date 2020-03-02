7fe57283bfec8ec4ed14f8eb937d9f8530e9cca4
RiskTolerance <isaac.druin@gmail.com>
2020-03-02 15:24:55 -0800

    Heroku

---
+++ app.js
@@ -0,0 +1,33 @@
+// Require Express
+const express = require("express");
+
+// Express server handling requests and responses
+const app = express();
+
+// Make everything inside of public/ available
+app.use(express.static("public"));
+
+// home route
+app.get("/", (request, response, next) => {
+  response.sendFile(__dirname + "/views/home.html");
+});
+
+// about route
+app.get("/about", (request, response, next) => {
+  response.sendFile(__dirname + "/views/about.html");
+});
+
+// works route
+app.get("/works", (request, response, next) => {
+  response.sendFile(__dirname + "/views/works.html");
+});
+
+// images route
+app.get("/images", (request, response, next) => {
+  response.sendFile(__dirname + "/views/images.html");
+});
+
+// Server Started
+app.listen(3000, () => {
+  console.log("My first app listening on port 3000!");
+});
